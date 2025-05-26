58ed20486aa44ceb052671f624ff1a793ce8e811
huangenjun <huangenjun@gdtwzc.com>
2025-05-26 15:44:06 +0800

    refactor(start-static-server): 局域网访问静态资源服务

--- start-static-server.js
+++ start-static-server.js
@@ -5,7 +5,7 @@ const port = 9002;
 
 app.use(express.static(path.join(__dirname, 'resources')));
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
   console.log(`静态资源服务已启动: http://localhost:${port}`);
   console.log(`可通过 http://localhost:${port}/wines.json 访问配置文件`);
 }); 
